51836d8d565d12c14efb22e99edd8a67eb423e5f
Ikeda <brandon_ikeda@hotmail.com>
2017-03-27 14:46:29 -0700

    Added half pyramid function

---
+++ halfPyramid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+  int i, j, rows;
+
+  printf("Please enter the number of rows you wish to have: ");
+  scanf("%d", &rows);
+
+  for(int i =1; i <= rows; ++i){
+    for(j=1; j <= i; ++j){
+      printf("* ");
+    }
+    printf("\git n");
+  }
+  return 0;
+}
