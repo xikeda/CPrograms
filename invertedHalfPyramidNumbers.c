a89b5a461595ba9a205802c54eee61ab98f8e694
Ikeda <brandon_ikeda@hotmail.com>
2017-03-27 15:05:12 -0700

    Added inverted half pyramid numbers function

---
+++ invertedHalfPyramidNumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+  int i, j, rows;
+
+  printf("Please enter the number of rows you wish to have: ");
+  scanf("%d", &rows);
+
+  for(i = rows; i >= 1; --i){
+    for(j=1; j <= i; ++j){
+      printf("%d ", git j);
+    }
+    printf("\n");
+  }
+  return 0;
+}
