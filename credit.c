00f6908abbd92c6ad3f1c94077f03180e3fa907e
Ikeda <brandon_ikeda@hotmail.com>
2017-04-03 14:33:37 -0700

    Second commit for credit.c

--- credit.c
+++ credit.c
@@ -4,19 +4,36 @@
 int main(void) {
     //Declare variables
     long long number;
+    int x = 1;
+    int y = 1;
     string card = " ";
 
-    //Get input, validate
-    do{
-    printf("Number: ");
-    number = get_long_long();
-    if(number < 1000000000000 || number > 10000000000000000){
-        printf("Go fuck yourself\n");
-    }
-    }while(number < 1000000000000 || number > 10000000000000000);
+        //Get input, validate
+        do{
+        printf("Number: ");
+        number = get_long_long();
+        }while(number < 1000000000000 || number > 10000000000000000);
 
+        /*
+        Luhn’s algorithm
+        Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products' digits together.
+        Add the sum to the sum of the digits that weren’t multiplied by 2.If the total’s last digit is 0 (or, put more formally,
+        if the total modulo 10 is congruent to 0), the number is valid!
+        */
+
+            //Determine card
+            if(){
+                printf("VISA\n");
+            }
+            else if(){
+                printf("MASTERCARD\n");
+            }
+            else if(){
+                printf("AMEX\n");
+            }
+            else{
+                printf("INVALID");
+            }
     //Return
-    printf("%s", card);
     return 0;
-
 }
